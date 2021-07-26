600c5e01969f6a1c2401013cd4bf51bf97d9910b
Andres Garcia <andresfgare@gmail.com>
2021-07-26 10:46:55 -0500

    App ok

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import Template from './templates/Template.js';
-console.log('hola');
-
+import Template from '@templates/Template.js';
+import '@styles/main.css';
+import '@styles/vars.styl';
+import '@styles/style.scss';
+console.log('Hola');
 (async function App() {
-  const main = null || document.getElementById('main');
-  main.innerHTML = await Template();
+    const main = null || document.getElementById('main');
+    main.innerHTML = await Template();
 })();
